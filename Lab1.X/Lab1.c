ab51d518bf0f5145f78192b39ae373cc96ce17f9
Jesse Whitworth (jazzywhit) <jessecwhitworth@gmail.com>
2012-09-20 16:10:31 -0400

    Same as last commit

--- Lab1.X/Lab1.c
+++ Lab1.X/Lab1.c
@@ -1,19 +1,28 @@
-/* UMass Lowell 16.480/552, Fall 2011
+/* UMass Lowell 16.480/552, Fall 2012
  *
  * Lab 1
  *
- * pic_adc_template.c
- * Template for PIC based ADC. 
+ * Lab1.c
  * 
  * License: GNU GPLv3.
  */
 
-/* Group Name: 
+/* Group Name: Gangstas
  * Group Members:
- * Date:
+ * - Jesse Whitworth
+ * - Matthew Cook
+ * - Denis Lemos
+ * - Aadil Hassan
+ * 
+ * Date: 9-20-2012
  */
 
+#include "p18f45K20.h"
+#pragma config FOSC = INTIO67
+#pragma config WDTEN = OFF, LVP = OFF
 
+
+//*** These are likely not needed as the new version of MPLABX has the precompilers already configured.
 ////////////////////////////////////////
 /* Toolchain-specific directives
 #ifdef SDCC
@@ -27,67 +36,87 @@
 	#define GO_DONE GODONE
 #endif
 
+/*
+ * DEFINITIONS ***** These should actually have no place in this program
+ */
 
+//Preprocessors
+#define PPBit0 RA2  // PP Nibble bits
+#define PPTRIS TRISA2=TRISC2=TRISC1=TRISC0 // PP Nibble tristate; allows setting all PP nibble bits as input or output simultaneously
 
-////////////////////////////////////////
-// Constants
-
-// PP Nibble bits
-#define PPBit0 RA2
-
-// PP Nibble tristate; allows setting all PP nibble
-//  bits as input or output simultaneously
-#define PPTRIS TRISA2=TRISC2=TRISC1=TRISC0
-
-////////////////////////////////////////
-// Structure Definitions
+//Structures
 struct bounds{
 	unsigned int low, high;
 };
 
-////////////////////////////////////////
-// Function Declarations
-unsigned int  readAN0();
+/*
+ * FUNCTION DECLARATIONS
+*/
+void Init();
+void InitADC();
 
-////////////////////////////////////////
-// Global Variables
-unsigned int adc=0;                        // Holds the value of the ADC after calling readAN0()
 
-////////////////////////////////////////
-// Function Definitions
-
-// readAN0
+/*
+ * ------------------readAN0
 // Purpose:    Reads the analog value of AN0 and stores it in "int adc"
 // Parameters: None; But the ANCON and ANSEL registers should be setup before this method is called
 // Returns:    The new value of "int adc"
-
-unsigned int readAN0(){
-	
-	return adc;
-}
 */
+unsigned int readAN0(){
+
+	return 0;
+}
 
 
-#include "p18f45K20.h"
-#pragma config FOSC = INTIO67
-#pragma config WDTEN = OFF, LVP = OFF
-/* main
- * Purpose: Initializes PIC (setting configuration / pin tristates) and then
- *           passes control to listenForCommands()
+
+/*
+ * ----------------------- I n i t () ---------------------------------------
+ * Initialize the board
+ * Functions called:
+ *  InitADC()
+ */
+void Init(){
+    // Comparitors off, CxIN pins are configured as digital I/O
+    
+    InitADC();
+}
+
+
+/*
+ * ----------------------- I n i t A D C () ---------------------------------
+ * Initialize any registers that the ADC needs for communication.
+ */
+void InitADC(){
+
+    // From the template
+    // We only need one analog pin: AN0
+    // Setup ADCON0 and other ADC related registers
+
+    // From the Programming guide p.45
+    //    Looking at the schematic of the 44-Pin Demo Board in the Appendix, the potentiometer (RP1) output is connected to the RA0/AN0 pin of the PIC18F45K20.
+    //    The basic steps needed to convert the ADC voltage on this pin are:
+    //    1. Configure the RA0/AN0 pin as an analog input in ANSEL.
+    //    2. Set the ADC voltage references in ADCON1.
+    //    3. Set the result justification, ADC clock source, and acquisition time in ADCON2.
+    //    4. Select the channel and turn on the ADC in ADCON0.
+    //    5. Start the conversion in ADCON0.
+}
+
+/*
+ * ----------------------- m a i n () ---------------------------------
  */
 void main(){	
 
-	TRISD = 0b01111111;// PORTD bit 7 to output (0); bits 6:0 are inputs (1)
-	LATDbits.LATD7 = 1;// Set LAT register bit 7 to turn on LED
-	while (1)
-	;
+    //Light LED Example code (sanity)
+    //TRISD = 0b01111111;// PORTD bit 7 to output (0); bits 6:0 are inputs (1)
+    //LATDbits.LATD7 = 1;// Set LAT register bit 7 to turn on LED
 
-	// Comparitors off, CxIN pins are configured as digital I/O
-	
-	// We only need one analog pin: AN0
-	
-	// Setup ADCON0 and other ADC related registers
-	
-	// Obtain ADC result
-       // readAN0();
+    //Initialize the board and all necesary ports
+    Init();
+
+    // Obtain ADC result
+    readAN0();
+
+    while (1)
+    ;
 }
