6a2ce0e49ba5a0d2bf18c762cfc22484c3cdf23c
root <root@ladmin-OptiPlex-GX620.(none)>
2012-10-18 12:32:58 -0400

    Strobe and Data are functioning. - Strobe is acting wonky - WriteByte seems fine

--- Lab2/test/paralleltext.c
+++ Lab2/test/paralleltext.c
@@ -5,37 +5,107 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define uchar unsigned char
+#define TRUE 1
+#define FALSE 0
+
+/* Parallel Port Constants */
 #define pport 0x378
+#define ppData pport
+#define ppStatus pport + 1
+#define ppStrobe pport + 2
+#define MSG_RESET 0x0
+#define MSG_PING 0x1
+#define MSG_GET 0x2
+#define MSG_ACK 0xE
+#define MSG_NOTHING 0xF
+
+/* Function Declarations */
+int OpenPort(int portAdr); //Open the port with the designated port number.
+int WriteByte(uchar byte); //Send a byte by breaking it into 4bit parts.
+int ParPortWrite(uchar byte);
+int ReadByte(uchar *byte); //Send a byte by breaking it into 4bit parts.
+int ParPortRead(uchar *byte);
+
+int OpenPort(int portAdr){
+	
+	printf("Opening Port: %X\n", portAdr);
+	if(ioperm(portAdr,3,1)) return FALSE;
+	return TRUE;
+}
+
+int WriteByte(uchar byte){
+	
+	ParPortWrite(byte>>4);
+	sleep(1); //Just so we can see it on the LCD.
+	ParPortWrite(byte & 0x0F);
+	
+	return TRUE;
+}
+
+int ReadByte(uchar *byte){
+	uchar a, b;
+	while(ParPortRead(&a));
+	sleep(2);
+	while(ParPortRead(&b));
+	
+	*byte = a<<4 | b; //Reconstruct the two 4bit messages into a byte
+	return TRUE;
+}
+
+int ParPortRead(uchar *byte){
+
+	outb(0x20, ppStrobe); //Set strobe low and bit 5 high for input mode.
+	outb(0x21, ppStrobe); //Set strobe high and input mode.
+	usleep(10000);
+	*byte = inb(ppData);
+	outb(0x20, ppStrobe);
+	return TRUE;
+}
+
+int ParPortWrite(uchar byte){
+	outb(0x00, ppStrobe); //Set the strobe low
+	sleep(3);
+	outb(byte, ppData); //Send the data
+	outb(0x01, ppStrobe); //Set the strobe high
+	//usleep(10000);
+	sleep(3);
+	outb(0x00, ppStrobe);
+	//Need to check for an ACK. This might require calling ParPortRead.
+	return TRUE;
+} 
 
 int main(int argc, char *argv[]){
 
-	//int parportfd = open("/dev/parport0", 0_RDWR);	
-	//if (result != 0) result = ioctl(parportfd,PPCLAIM);
+	int i = 0;
+	uchar dataHex;
+
 	if (argc < 2){
-		printf("Enter a two digit hex value (0F, FF, 01, etc..)\n");
+		printf("Data Hex Missing: Enter a two digit hex value (0F, FF, 01, etc..)\n");
 		return 1;
 	}
-	char hexChar;
-	hexChar = strtol(argv[1], NULL, 16);
-	//hex_value[1] = strtol(argv[1], NULL, 16)*16;
-	printf("You entered: %X\n", hexChar);
-	//return 1;
-	if(ioperm(pport,3,1)) return(1);
-	int i = 0;
-	for(;i < 10000; i++){
-		char a, b, c; 
+ 
+	dataHex = strtol(argv[1], NULL, 16);
 
-		outb(0x01,pport);
-		outb(0x00,pport+2); //Strobe
-		a = inb(pport);	
-		b = inb(pport+1);	
-		c = inb(pport+2);	
-		printf("%X\t%X\t%X\n",a, b, c);
-		//printf("%X\t\n",a);
-		usleep(1000000);
-	 	
+	printf("DataHex: %X\n", dataHex);
+
+	if(OpenPort(pport)){
+		printf("Port Opened!\n");
+	} else {
+		printf("Port Could not be opened. Ensure root access.\n");
 	}
 
-	//result = ioctl(parportfd,PPRELEASE);
-	//close(parportfd);
+	for(;i < 1; i++){
+		uchar a, b, c; 
+		WriteByte(dataHex); 	
+	
+		//a = inb(pport);	
+		//b = inb(pport+1);	
+		//c = inb(pport+2);	
+		//printf("%X\t%X\t%X\n",a, b, c);
+		//printf("%X\t\n",a);
+		//usleep(1000000);
+	}
 }
+
+
