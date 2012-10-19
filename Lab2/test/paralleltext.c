49589fde6063d271e916d1f555cee3008b599de9
root <root@ladmin-OptiPlex-GX620.(none)>
2012-10-19 19:17:37 -0400

    Restructure of Linux Program for Lab 2

--- Lab2/test/paralleltext.c
+++ Lab2/test/paralleltext.c
@@ -4,8 +4,9 @@
 #include <sys/io.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <ctype.h>
 
-#define uchar unsigned char
+#define BYTE unsigned char
 #define TRUE 1
 #define FALSE 0
 
@@ -22,19 +23,169 @@
 
 /* Function Declarations */
 int OpenPort(int portAdr); //Open the port with the designated port number.
-int WriteByte(uchar byte); //Send a byte by breaking it into 4bit parts.
-int ParPortWrite(uchar byte);
-int ReadByte(uchar *byte); //Send a byte by breaking it into 4bit parts.
-int ParPortRead(uchar *byte);
+int WriteByte(BYTE byte); //Send a byte by breaking it into 4bit parts.
+int ParPortWrite(BYTE byte);
+BYTE ReadByte(); //Send a byte by breaking it into 4bit parts.
+BYTE ParPortRead();
+int Reset();
+int Get();
+int Ping();
+void DisplayData( BYTE RTCData[], int adcResult ) ; 
 
-int OpenPort(int portAdr){
-	
-	printf("Opening Port: %X\n", portAdr);
-	if(ioperm(portAdr,3,1)) return FALSE;
-	return TRUE;
+void DisplayData( BYTE RTCData[], int adcResult ) {
+  printf("\n\n\nMSG_GET Results\n---------------------------\n");
+
 }
 
-int WriteByte(uchar byte){
+    BYTE ReadByte(){
+
+    BYTE temp;
+    BYTE byte;
+    
+    temp = ParPortRead();
+    byte = temp<<4 & 0xF0; //Reconstruct the two 4bit messages into a byte
+    temp = ParPortRead();
+    byte |= temp;
+    
+    return byte;
+  
+  }
+
+
+int Get(){
+ 
+  printf("I am fucking getting");
+
+  BYTE RTCData[8];
+  BYTE adcHighNible; 
+  BYTE adcLowByte;
+  int adcResult;
+
+  // Send Command
+  ParPortWrite(MSG_GET);
+
+   // Read 3 Nible from ADC 
+  adcHighNible = ParPortRead();
+  adcLowByte = ReadByte();
+  adcResult = ((adcHighNible&0x0F) * 16 * 16) + adcLowByte ;
+
+  // Read RTC data
+  int i ;
+  for(i = 0 ; i < 8 ; i++) {
+    RTCData[i] = ReadByte() ;
+  }
+
+  // Get Acknowledgement 
+  if(!Ping()) return FALSE ;
+
+  // Display Data 
+  DisplayData( RTCData , adcResult );
+ 
+  return TRUE;
+}
+
+int Reset(){
+
+  BYTE result ;
+
+  printf("\nI am fucking resetting");
+  ParPortWrite(MSG_RESET);
+  result = ParPortRead();
+
+  if( result != MSG_RESET ) return FALSE ;
+
+  return TRUE ; 
+
+}
+
+
+int Ping(){
+
+  printf("\nI and fucking pinging");
+  BYTE result ;
+
+  ParPortWrite(MSG_PING);
+  result = ParPortRead();
+
+  if( result != MSG_ACK ) return FALSE ;
+
+  return TRUE ;
+
+}
+
+
+int OpenPort(int portAdr){
+  
+  printf("Opening Port: %X\n", portAdr);
+  if(ioperm(portAdr,3,1)) return FALSE;
+  return TRUE;
+}
+
+
+BYTE ParPortRead(){
+  BYTE byte;
+  outb(0x21, ppStrobe); //Set strobe low and bit 5 high for input mode.
+  outb(0x20, ppStrobe); //Set strobe high and input mode.
+  usleep(10000);
+  byte = inb(ppData);
+  outb(0x21, ppStrobe);  //Set strobe low and bit 5 high for input mode.
+  return byte;
+}
+
+int ParPortWrite(BYTE byte){
+  outb(0x01, ppStrobe); //Set the strobe low
+  outb(byte, ppData); //Output command to data bus.
+  outb(0x00, ppStrobe); //Set the strobe high
+  sleep(10000);
+  outb(0x01, ppStrobe); // Set the strobe to low.
+  return TRUE;
+} 
+
+int main(int argc, char *argv[]){
+  int scanfresult;
+  char cmd;
+  int Yay = 1;
+  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"); //Don't you fucking dare take this out.
+  if(OpenPort(pport)){
+    printf("Port Opened!\n-------------------------\n");
+  } else {
+    printf("Port Could not be opened. Ensure root access.\n-------------------------\n");
+    return(-1);
+  }
+  printf("\nCommands:\n\tR = Reset\n\tG = Get\n\tP = Ping\n\tQ = Quit\n");
+  while(1){
+    
+    printf("\nEnter a command: ");
+    
+    scanfresult = scanf("%c",&cmd);
+    while (getchar() != '\n');
+    switch(toupper(cmd)){
+    case 'R':
+      Yay = Reset();
+      break;
+    case 'G':
+      Yay = Get();
+      break;
+    case 'P':
+      printf("before");
+      Yay = Ping();
+      printf("after");
+      break;
+    case 'Q':
+      return(0);
+    default:
+      Yay=FALSE;
+      break;
+    }
+    if(!Yay)
+      {
+	printf("Enter another command. (R,G,P,Q)");
+      }
+  }
+  return(0);
+}
+
+int WriteByte(BYTE byte){
 	
 	ParPortWrite(byte>>4);
 	sleep(1); //Just so we can see it on the LCD.
@@ -43,53 +194,9 @@ int WriteByte(uchar byte){
 	return TRUE;
 }
 
-int ReadByte(uchar *byte){
-	uchar a, b;
-	while(ParPortRead(&a));
-	//sleep(2);
-	//while(ParPortRead(&b));
-	
-	*byte = a<<4 | b; //Reconstruct the two 4bit messages into a byte
-	return TRUE;
-}
-
-int ParPortRead(uchar *byte){
-
-	outb(0x20, ppStrobe); //Set strobe low and bit 5 high for input mode.
-	outb(0x21, ppStrobe); //Set strobe high and input mode.
-	usleep(10000);
-	*byte = inb(ppData);
-	outb(0x20, ppStrobe);
-	return TRUE;
-}
-
-int ParPortWrite(uchar byte){
-	outb(0x00, ppStrobe); //Set the strobe low
-	//printf("%X\n", inb(ppStrobe));
-	sleep(3);
-	outb(0x01, ppStrobe); //Set the strobe high
-	sleep(3);
-	outb(0x00, ppStrobe); //Set the strobe low
-	//printf("%X\n", inb(ppStrobe));
-	sleep(3);
-	outb(0x01, ppStrobe); //Set the strobe high
-	sleep(3);
-	//outb(0x01, ppStrobe); //Set the strobe low
-	//outb(byte, ppData); //Send the data	
-	//outb(0x01, ppStrobe); //Set the strobe high
-	//printf("%X\n", inb(ppStrobe));
-	//usleep(10000);
-	//sleep(3);
-	//outb(0x00, ppStrobe);
-	//printf("%X\n", inb(ppStrobe));
-	//Need to check for an ACK. This might require calling ParPortRead.
-	return TRUE;
-} 
-
-int main(int argc, char *argv[]){
-
+  /*
 	int i = 0;
-	uchar dataHex;
+	char dataHex;
 
 	if (argc < 2){
 		printf("Data Hex Missing: Enter a two digit hex value (0F, FF, 01, etc..)\n");
@@ -106,8 +213,8 @@ int main(int argc, char *argv[]){
 		printf("Port Could not be opened. Ensure root access.\n");
 	}
 
-	for(;i < 1; i++){
-		uchar a, b, c; 
+	//for(;i < 1; i++){
+	//	byte a, b, c; 
 	ParPortWrite(dataHex); 	
 	
 		//a = inb(pport);	
@@ -116,5 +223,5 @@ int main(int argc, char *argv[]){
 		//printf("%X\t%X\t%X\n",a, b, c);
 		//printf("%X\t\n",a);
 		//usleep(1000000);
-	}
-}
+	//}
+	*/
